a0e501eba121c7c93326918643de9f6bed5a38f7
abhi <abhigowdabs154@gmail.com>
2024-06-27 19:54:01 +0530

    cart, headerRight for top right button, stack screenoprion to make headerright button visible acroos all screen

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const cart = () => {
+import { View, Text ,Platform} from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+ 
+const cartScreen = () => {
   return (
     <View>
-      <Text>cart</Text>
+      <Text style={{color:"red"}}>cart</Text>
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+
     </View>
   )
 }
 
-export default cart
+export default cartScreen;
