3c0ad1be2d282728577a5fa9c5296f5d00a029db
abhi <abhigowdabs154@gmail.com>
2024-07-20 06:22:43 +0530

    orders topTab using react navigation

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from "expo-router";
 export default function OrderLayaout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Orders" }} />
+      <Stack.Screen name="list" options={{  headerShown:false}} />
     </Stack>
   ); 
 };
--- src/app/(admin)/orders/list/_layout.tsx
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -1,5 +1,17 @@
-import { Stack } from "expo-router"
+import { withLayoutContext } from 'expo-router';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export const TopTabs = withLayoutContext(
+  createMaterialTopTabNavigator().Navigator
+);
 
 export default function OrdersTabs() {
-    return <Stack/>
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }} edges={['top']}>
+      <TopTabs>
+        <TopTabs.Screen name="index" options={{ title: 'Active' }} />
+      </TopTabs>
+    </SafeAreaView>
+  );
 }
